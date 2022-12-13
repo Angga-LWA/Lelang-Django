dd6023a7e893458d3d34db3877ba78f932209923
Yoga Anggara <doctorbean31@gmail.com>
2022-12-13 14:38:10 +0700

    create model
    Users Model

--- devlelang/lelang/models.py
+++ devlelang/lelang/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+    userId = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=20)
+    birthday = models.CharField(max_length=50)
+    birthplace = models.CharField(max_length=50)
+    address_id = models.CharField(max_length=50)
+    address_domisili = models.CharField(max_length=50)
+    profile_img = models.CharField(max_length=500)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    flag_active = models.CharField(max_length=50)
+    nik = models.CharField(max_length=100)
+    npwp = models.CharField(max_length=100)
+    created_by = models.CharField(max_length=100)
+
+
